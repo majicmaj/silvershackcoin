12b6d808aead94ae896e51b7ede7a1f368461d6f
majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-01-23 15:38:59 -0500

    added listings.json and adjusted app and styling &

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, { Component } from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import listings from "./listings.json";
 
-let listings = [
-  {
-    id: 0,
-    title: "Silver Indian Head Round Bullion",
-    blurb:
-      "1 Gram .999 Fine Silver Fractional Bullion Round -Indian Head Design",
-    img: "https://i.ebayimg.com/images/g/6i0AAOSwnyNdK8OP/s-l500.jpg",
-    price: 1.99
-  },
-  {
-    id: 1,
-    title: "1964 Kenndefy Half Dollar",
-    blurb: "1964 Kennedy Half Dollar Brilliant Uncirculated - BU",
-    img: "https://i.ebayimg.com/images/g/jdcAAOSwcrdcoaC5/s-l1600.jpg",
-    price: 11.95
-  },
-  {
-    id: 2,
-    title: "Unsearched Half Dollar Roll",
-    blurb: "1 Unsearched, Bank Sealed Half Dollar Roll",
-    img: "https://i.ebayimg.com/images/g/p4cAAOSwDcJcYxz9/s-l1600.jpg",
-    price: 17.87
-  }
-];
 class App extends Component {
   constructor(props) {
     super();
@@ -96,8 +73,8 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <div className="Listings">
-          <p>Listings</p>
+        <div className="Nav">
+          <h1>Silver Shack Coins</h1>
           <input
             type="text"
             value={this.state.searchString}
@@ -105,52 +82,49 @@ class App extends Component {
             onChange={this.handleChange}
             placeholder="search for an item"
           />
-          <ul>
-            {_listings.map(item => (
-              <li
-                key={item.id}
-                id={item.id}
-                style={{ border: "1px solid black" }}
-              >
-                <img alt="" src={item.img} style={{ maxHeight: "100px" }} />
-                <h3>{item.title}</h3>
-                <p>{item.blurb}</p>
-                <p>price: US ${item.price}</p>
-                <button onClick={() => this.addToCart(item.id)}>
-                  add to cart
-                </button>
-              </li>
-            ))}
-          </ul>
         </div>
-
-        <div className="Cart">
-          <p>Cart</p>
-          <p>{this.state.cart.toStrting}</p>
-          <ul>
-            {_cart.map(item => {
-              if (item.quantity > 0) {
-                return (
-                  <li
-                    key={item.id}
-                    id={item.id}
-                    style={{ border: "1px solid black" }}
-                  >
-                    <img alt="" src={item.img} style={{ maxHeight: "100px" }} />
-                    <p>{item.title}</p>
-                    <p>Quantity:{item.quantity}</p>
-                    <button onClick={() => this.changeQuantity(item.id, -1)}>
-                      -
+        <div className="body">
+          <div className="Listings">
+            <h3>Listings</h3>
+            <ul>
+              {_listings.map(item => (
+                <li className="item listing" key={item.id} id={item.id}>
+                  <img alt="" src={item.img} />
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.blurb}</p>
+                    <p>price: US ${item.price}</p>
+                    <button onClick={() => this.addToCart(item.id)}>
+                      add to cart
                     </button>
-                    <button onClick={() => this.changeQuantity(item.id, 1)}>
-                      +
-                    </button>
-                  </li>
-                );
-              } else return "";
-            })}
-          </ul>
-          <button>Check Out</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="Cart">
+            <h3>Cart</h3>
+            <ul>
+              {_cart.map(item => {
+                if (item.quantity > 0) {
+                  return (
+                    <li className="item cartitem" key={item.id} id={item.id}>
+                      <img alt="" src={item.img} />
+                      <p>{item.title}</p>
+                      <p>Quantity:{item.quantity}</p>
+                      <button onClick={() => this.changeQuantity(item.id, -1)}>
+                        -
+                      </button>
+                      <button onClick={() => this.changeQuantity(item.id, 1)}>
+                        +
+                      </button>
+                    </li>
+                  );
+                } else return "";
+              })}
+            </ul>
+            <button>Check Out</button>
+          </div>
         </div>
       </div>
     );
