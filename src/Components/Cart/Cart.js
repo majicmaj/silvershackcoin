6794b42bd56d55b95875a2106a193ec6c945b14e
majicmaj <52975556+majicmaj@users.noreply.github.com>
2020-01-30 17:37:38 -0500

    Fixed main listing responsiveness and styling
    -fixed responsive listing view overlap

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -11,19 +11,30 @@ class Cart extends Component {
   render() {
     return (
       <div className="Cart">
-        <h3>Cart</h3>
-        <ul>
+        <div className="labels">
+          <p>Cart</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Adjust</p>
+          <p>Total</p>
+        </div>
+        <div className="items">
           {this.props._cart.map(item => {
             if (item.quantity > 0) {
               return (
-                <li className="item" key={item.id} id={item.id}>
+                <div className="item" key={item.id} id={item.id}>
                   <img alt="" src={item.img} />
                   <div>
                     <p>{item.title}</p>
-                    <p>Price: ${item.price / 100}</p>
-                    <p>Quantity: {item.quantity}</p>
+                    <p>{item.blurb}</p>
                   </div>
-                  <div className="buttons">
+                  <div>
+                    <p>${item.price / 100}</p>
+                  </div>
+                  <div>
+                    <p>{item.quantity}</p>
+                  </div>
+                  <div>
                     <button
                       className="minus"
                       onClick={() => this.props.changeQuantity(item.id, -1)}
@@ -36,21 +47,62 @@ class Cart extends Component {
                       <i className="fas fa-plus" />
                     </button>
                   </div>
-                </li>
+                  <div>${(item.price * item.quantity) / 100}</div>
+                </div>
               );
-            } else return "";
+            } else return null;
           })}
-        </ul>
-        <p>
-          Total: $<span className="total">{this.props.total / 100}</span>
-        </p>
-
-        <Link to="/checkout">
-          <button className="checkout" onClick={this.props.handleCheckout}>
-            Check Out
-          </button>
-        </Link>
+        </div>
+        <div>
+          <p>Subtotal: {this.props.total / 100}</p>
+          <p>Tax: {(this.props.total / 100) * 0.05}</p>
+          <p>Shipping: $15:00</p>
+          <p>
+            Total: {this.props.total / 100 + (this.props.total / 100) * 0.05}
+          </p>
+        </div>
       </div>
+      // <div className="Cart">
+      //   <h3>Cart</h3>
+      //   <ul>
+      //     {this.props._cart.map(item => {
+      //       if (item.quantity > 0) {
+      //         return (
+      //           <li className="item" key={item.id} id={item.id}>
+      //             <img alt="" src={item.img} />
+      //             <div>
+      //               <p>{item.title}</p>
+      //               <p>Price: ${item.price / 100}</p>
+      //               <p>Quantity: {item.quantity}</p>
+      //             </div>
+      //             <div className="buttons">
+      //               <button
+      //                 className="minus"
+      //                 onClick={() => this.props.changeQuantity(item.id, -1)}
+      //               >
+      //                 <i className="fas fa-minus" />
+      //               </button>
+      //               <button
+      //                 onClick={() => this.props.changeQuantity(item.id, 1)}
+      //               >
+      //                 <i className="fas fa-plus" />
+      //               </button>
+      //             </div>
+      //           </li>
+      //         );
+      //       } else return "";
+      //     })}
+      //   </ul>
+      //   <p>
+      //     Total: $<span className="total">{this.props.total / 100}</span>
+      //   </p>
+
+      //   <Link to="/checkout">
+      //     <button className="checkout" onClick={this.props.handleCheckout}>
+      //       Check Out
+      //     </button>
+      //   </Link>
+      // </div>
     );
   }
 }
